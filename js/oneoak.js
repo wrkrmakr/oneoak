4fd80a9f1fb4d607b9abcb787436e1b6a7492fdb
Jeff Quick <delrinbobbins@yahoo.ca>
2013-10-02 17:26:55 -0400

    start rebuild for animations

--- js/oneoak.js
+++ js/oneoak.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
 
-  $('.btn').click(function(element){
-    element.preventDefault();
+  $('.btn').click(function(event){
+    event.preventDefault();
 
     $('.innerPanel div').removeClass('visible').addClass('hidden');
     $('.innerPanel #' + this.id).removeClass('hidden').addClass('visible');
@@ -11,5 +11,30 @@ $( document ).ready(function() {
 
     $('.circle-btn').children('img').attr('src', 'img/plus.png');
     $(this).children('.circle-btn').children('img').attr('src', 'img/minus.png');
+  });
+
+  $('.nav-btn').click(function(event){
+    event.preventDefault();
+    $('.nav-btn').addClass('nav-btn-inactive').removeClass('nav-btn-active');
+    $('.nav-btn-vision').addClass('nav-btn-inactive').removeClass('nav-btn-active');
+    $(this).addClass('nav-btn-active').removeClass('nav-btn-inactive');
+
+    // showing sidebar associated with the nav button clicked
+	var sidebar_target = $(this).data('sidebarTarget');
+	$('.sidebar-inner').hide();
+	$(sidebar_target).show();
+  });
+
+ $('.nav-btn-vision').click(function(event){
+    event.preventDefault();
+
+    // highlighting active nav button 
+    $('.nav-btn').addClass('nav-btn-inactive').removeClass('nav-btn-active');
+    $(this).addClass('nav-btn-active').removeClass('nav-btn-inactive');
+
+    // showing sidebar associated with the nav button clicked
+	var sidebar_target = $(this).data('sidebarTarget');
+	$('.sidebar-inner').hide();
+	$(sidebar_target).show();
   });
 });
