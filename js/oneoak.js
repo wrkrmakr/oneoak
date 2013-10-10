86c73591528591d5c433e64f975475b51a701ae6
chris butcher <cbutcher@gmail.com>
2013-10-10 16:05:14 -0400

    Animating navigation

--- js/oneoak.js
+++ js/oneoak.js
@@ -1,13 +1,29 @@
 // Slide menu up from bottom using negative margin
 // http://jsfiddle.net/SpMns/
 
-$( document ).ready(function() {
+
+$(function(){
 
     var hash=location.hash.replace('#', '');
-    // $('.sidebar').show();
-    $('#' + 'sidebar-' + hash).show();
 
-    // $('#' + 'sidebar-' + hash).switchClass('sidebar-inner-minimized', 'sidebar-inner', 2000);
+    // $('#' + 'sidebar-' + hash).stop(true).animate({ 'marginBottom': 0 }, { queue: false, duration: 1200 });
+    
+    // $('#' + 'sidebar-' + hash).attr('margin-bottom', '-300px');
+
+
+    // $('#' + 'sidebar-' + hash).animate({'margin-bottom':"0px"}, 1500);
+
+    // alert(hash);
+
+    // $('#' + 'sidebar-' + hash).attr({'margin-bottom':"-500px"});
+    
+    // $('#' + 'sidebar-' + hash).animate({'margin-bottom':"-500px"}, 1500);
+    
+    // $('#' + 'sidebar-' + hash).show();
+
+    // $('.sidebar').show();
+    // $('#' + 'sidebar-' + hash).show();
+
     // $('.sidebar').switchClass('sidebar-parent-minimized',    'sidebar-parent', 2000);
 
     // $('#' + 'sidebar-' + hash).hide();
@@ -34,36 +50,49 @@ $( document ).ready(function() {
     $('.nav-btn').click(function(event){
         event.preventDefault();
 
+        var sidebar_target = $(this).data('sidebarTarget');
         $('.panel').fadeOut();
 
         $('div.label').addClass('inactive').removeClass('active');
         $('.circle-btn').children('img').attr('src', 'img/plus.png');
 
         $('.nav-btn').addClass('nav-btn-inactive').removeClass('nav-btn-active');
-        $('.nav-btn-vision').addClass('nav-btn-inactive').removeClass('nav-btn-active');
         $(this).addClass('nav-btn-active').removeClass('nav-btn-inactive');
 
-        // showing sidebar associated with the nav button clicked
-        var sidebar_target = $(this).data('sidebarTarget');
-        $('.sidebar-inner').hide();
-        $(sidebar_target).show();
+        
+        $('.sidebar-inner').animate({ 'marginBottom': -520 }, { queue: false, duration: 1200, complete: function(){
+
+            
+            $('.sidebar-inner').hide();
+
+            // alert(sidebar_target);
+
+            $(sidebar_target).show();
+            $(sidebar_target).animate({ 'marginBottom': 0 }, { queue: false, duration: 1200 });
+        }});
+
+        
+
+        // // showing sidebar associated with the nav button clicked
+
+        // 
     });
 
-    $('.nav-btn-vision').click(function(event){
-        event.preventDefault();
+    // $('.nav-btn-vision').click(function(event){
+    //     event.preventDefault();
 
-        $('.panel').fadeOut();
+    //     $('.panel').fadeOut();
 
-        $('div.label').addClass('inactive').removeClass('active');
-        $('.circle-btn').children('img').attr('src', 'img/plus.png');
+    //     $('div.label').addClass('inactive').removeClass('active');
+    //     $('.circle-btn').children('img').attr('src', 'img/plus.png');
 
-        // highlighting active nav button 
-        $('.nav-btn').addClass('nav-btn-inactive').removeClass('nav-btn-active');
-        $(this).addClass('nav-btn-active').removeClass('nav-btn-inactive');
+    //     // highlighting active nav button 
+    //     $('.nav-btn').addClass('nav-btn-inactive').removeClass('nav-btn-active');
+    //     $(this).addClass('nav-btn-active').removeClass('nav-btn-inactive');
 
-        // showing sidebar associated with the nav button clicked
-        var sidebar_target = $(this).data('sidebarTarget');
-        $('.sidebar-inner').hide();
-        $(sidebar_target).show();
-    });
+    //     // showing sidebar associated with the nav button clicked
+    //     var sidebar_target = $(this).data('sidebarTarget');
+    //     $('.sidebar-inner').hide();
+    //     $(sidebar_target).show();
+    // });
 });
