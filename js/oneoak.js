c6054063fe8bba36436bf6eb4bca532bab039654
chris butcher <cbutcher@gmail.com>
2013-10-15 15:27:40 -0400

    Con_mgmt panel longer

--- js/oneoak.js
+++ js/oneoak.js
@@ -1,6 +1,9 @@
 // Slide menu up from bottom using negative margin
 // http://jsfiddle.net/SpMns/
 
+// Portfolio slideshow
+// http://www.pixedelic.com/plugins/camera/
+
 $('.btn').click(function(event){
     event.preventDefault();
 
@@ -10,6 +13,8 @@ $('.btn').click(function(event){
 
         // Panel, nor portfolio_panel are not visible
         if ($('.panel').css('display') === 'none' && $('#portfolio_panel').css('display') === 'none'){
+
+            adjustIfConMgmt(that);
 
             if (that.id === 'portfolio'){ // But portfolio is clicked first
                 toggleButton(that);
@@ -46,9 +51,10 @@ $('.btn').click(function(event){
             }
             else {
                 toggleButton(that);
-                $('.panel').toggle({effect: 'fold', complete: function(){
+                $('.panel').toggle({effect: 'fold', complete: function(){ // fold
                     switchPanel(that);
-                    $('.panel').toggle('fold');
+                    adjustIfConMgmt(that);
+                    $('.panel').toggle('fold'); // unfold
                 }});
             }
         }       
@@ -74,6 +80,15 @@ $('.btn').click(function(event){
         } 
     }
 });
+
+var adjustIfConMgmt = function(that){
+    if (that.id === 'con_mgmt'){
+        $(".panel").css('height', 280);
+    }
+    else {
+        $(".panel").css('height', 230);
+    }
+}
 
 var switchPanel = function(that){
     $('.innerPanel div').removeClass('visible').addClass('hidden');
@@ -109,30 +124,30 @@ $('.nav-btn').click(function(event){
         $(this).addClass('nav-btn-active').removeClass('nav-btn-inactive');
         
         if ($('.panel').css('display') === 'none' && $('#portfolio_panel').css('display') === 'none'){
-            $('.sidebar-inner').animate({ 'marginBottom': -520 }, { queue: false, duration: 600, complete: function(){
+            $('.sidebar-inner').animate({ 'marginBottom': -520 }, { queue: false, duration: 400, complete: function(){
                 $('.sidebar-inner').hide();
 
                 $(sidebar_target).show();
-                $(sidebar_target).animate({ 'marginBottom': 0 }, { queue: false, duration: 600 });
+                $(sidebar_target).animate({ 'marginBottom': 0 }, { queue: false, duration: 400 });
             }});
         }
         else if ($('.panel').css('display') != 'none' && $('#portfolio_panel').css('display') === 'none'){
             $('.panel').toggle({effect: 'fold', complete: function(){
-                $('.sidebar-inner').animate({ 'marginBottom': -520 }, { queue: false, duration: 600, complete: function(){
+                $('.sidebar-inner').animate({ 'marginBottom': -520 }, { queue: false, duration: 400, complete: function(){
                     $('.sidebar-inner').hide();
 
                     $(sidebar_target).show();
-                    $(sidebar_target).animate({ 'marginBottom': 0 }, { queue: false, duration: 600 });
+                    $(sidebar_target).animate({ 'marginBottom': 0 }, { queue: false, duration: 400 });
                 }});
             }});
         }
         else if ($('.panel').css('display') === 'none' && $('#portfolio_panel').css('display') != 'none'){
             $('#portfolio_panel').toggle({effect: 'fold', complete: function(){
-                $('.sidebar-inner').animate({ 'marginBottom': -520 }, { queue: false, duration: 600, complete: function(){
+                $('.sidebar-inner').animate({ 'marginBottom': -520 }, { queue: false, duration: 400, complete: function(){
                     $('.sidebar-inner').hide();
 
                     $(sidebar_target).show();
-                    $(sidebar_target).animate({ 'marginBottom': 0 }, { queue: false, duration: 600 });
+                    $(sidebar_target).animate({ 'marginBottom': 0 }, { queue: false, duration: 400 });
                 }});
             }});
         }
@@ -151,7 +166,7 @@ $(function () {
     $('#' + 'sidebar-' + hash).show();
 
     $('#' + 'sidebar-' + hash).css('marginBottom', -520)
-    $('#' + 'sidebar-' + hash).stop(true).animate({ 'marginBottom': 0 }, { queue: false, duration: 1500});
+    $('#' + 'sidebar-' + hash).stop(true).animate({ 'marginBottom': 0 }, { queue: false, duration: 400});
     
     $('#nav-btn-' + hash).addClass('nav-btn-active').removeClass('nav-btn-inactive');
 
