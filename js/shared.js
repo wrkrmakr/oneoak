4d3b0ca0a8886325ee14f31476e7698cc55bcb22
Jeff Quick <delrinbobbins@yahoo.ca>
2013-10-18 14:12:35 -0400

    tweaks to image carousel

--- js/shared.js
+++ js/shared.js
@@ -4,10 +4,10 @@ backgrounds[1] = 'img/oneoak-3.jpg';
 backgrounds[2] = 'img/oneoak-9.jpg';
 
 var siteContentBackgrounds = new Array();
-siteContentBackgrounds[0] = 'img/construction.jpg';
+siteContentBackgrounds[0] = 'img/oneoak-1.jpg';
 
 var min = 0;
-var max = 4;
+var max = 2;
 
 $.fn.preload = function(complete) {
   this.each(function(){
